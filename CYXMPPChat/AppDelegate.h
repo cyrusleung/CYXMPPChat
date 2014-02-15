3e9d1e92e78347f4ab0939e9ece16b7697a4f58f
FatKa Leung <FatKa@FatKatekiMac-mini.local>
2014-02-15 19:53:36 +0800

    Initial Commit

---
+++ CYXMPPChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CYXMPPChat
+//
+//  Created by FatKa Leung on 14-2-15.
+//  Copyright (c) 2014年 CYDESIGN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CYXMPPChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CYXMPPChat
+//
+//  Created by FatKa Leung on 14-2-15.
+//  Copyright (c) 2014年 CYDESIGN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
